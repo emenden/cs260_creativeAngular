08d107950d8acef93c0b308f4d28723f721849b0
eprigmore <eprigmore1@gmail.com>
2017-10-18 18:02:46 +0000

    added done event functionality

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ angular.module('ToDo',[])
    '$scope', function($scope) {
 	$scope.events = [];
 	$scope.addEvent = function() {
-	  $scope.events.push({toDo: $scope.formEvent, day: $scope.formDay});
+	  $scope.events.push({toDo: $scope.formEvent, day: $scope.formDay, checked: false});
 	  $scope.formEvent = '';
 	  $scope.formDay = '';
 	  console.log($scope.events);
-        }
+        };
+	$scope.doneEvent = function(event) {
+	  event.checked = true;
+	};
     }
     ]);
